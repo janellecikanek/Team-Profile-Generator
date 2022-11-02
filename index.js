b78d767a35ea4409f901e33ddece6974c2532aac
Janelle Cikanek <janellecikanek@gmail.com>
2022-11-02 18:54:27 -0500

    added testing files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+const generateHTML = ({ TeamMgrName, TeamMgrID, TeamMgrEmail, TeamMgrOfficeNumber, officeNumber, EngName, EngID, EngEmail, EngGithub, InternName, internID, InternEmail, internSchool }) =>
     `<!DOCTYPE html>
     <html lang="en">
       <head>
@@ -63,33 +63,76 @@ inquirer
     .prompt([
         {
             type: 'input',
-            name: 'name',
-            message: '"What is the name of your Team Manager?',
+            name: 'TeamMgrName',
+            message: "Team manager's name?",
         },
         {
             type: 'input',
-            name: 'employeeID',
-            message: 'Where are you from?',
+            name: 'TeamMgrID',
+            message: "Team manager's employee ID?",
         },
         {
             type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
+            name: 'TeamMgrEmail',
+            message: "Team manager's email",
         },
         {
             type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
+            name: 'TeamMgrOfficeNumber',
+            message: "Team manager's office number?",
+        },
+        {
+            type: 'list',
+            name: 'TeamMgrOfficeNumber',
+            message: "Would you like to add another engineer or intern?:",
+            Choices: ["Engineer", "Intern", "No"]
         },
         {
             type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
+            name: 'EngName',
+            message: "Engineer's name?",
         },
         {
             type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
+            name: 'EngID',
+            message: "Engineer's ID?",
+        },
+        {
+            type: 'input',
+            name: 'EngEmail',
+            message: "Engineer's email",
+        },
+        {
+            type: 'input',
+            name: 'EngGithub',
+            message: "Engineer's GitHub username?",
+        },
+
+        {
+            type: 'input',
+            name: 'InternName',
+            message: "Enter your intern's name."
+        },
+        {
+            type: 'input',
+            name: 'internID',
+            message: "Enter your intern's ID.",
+        },
+        {
+            type: 'input',
+            name: 'InternEmail',
+            message: "Enter your intern's email."
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "Enter your intern's school.",
+        },
+        {
+            type: 'list',
+            name: 'TeamMgrOfficeNumber',
+            message: "Would you like to add another engineer or intern?:",
+            Choices: ["Engineer", "Intern", "No"]
         },
     ])
     .then((answers) => {
